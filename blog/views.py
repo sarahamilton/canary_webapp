5c8daa50adb159d328327c8e5dcc450df8a52591
Sara Hamilton <sarahamilton@saras-macbook.home>
2019-04-24 10:39:21 +0100

    added blog template to render posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # A view is a function that returns something -- in this case HTML
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts}) # everything from user, view, params
+    
